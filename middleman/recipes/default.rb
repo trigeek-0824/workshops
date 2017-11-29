ef4e9fc9d9d1f9a4574dd9be77abb622492240c0
Robert Whitcher <whitcher_robert@yahoo.com>
2017-11-29 06:46:26 -0600

    Updated gemfile for middleman to work with Ruby 2.3
    As Ruby 2.1 is EOL/EOS I installed Ruby 2.3 instead
    However two of the gems used by middleman are incompatible with
       Ruby 2.2+
    Updated the Gemfile.lock:
        to update JSON to 1.8.3
        to update Eventmachine to 1.0.4
    
    Also I would note that while not listed in the install instructions
    'build-essential' seems to be required as some of the gems themselves
    use C++ and are compiled during build-install which requires g++
    
    Added build-essential to the installer.

--- middleman/recipes/default.rb
+++ middleman/recipes/default.rb
@@ -8,16 +8,24 @@ include_recipe "poise-ruby"
 ###
 # Install base packages
 #
+# Note: While not mentioned in the instructions 'build-essential' is required
+#       as some of the gems require C++ compilation
+#
 ###
 package [ 'openssl', 'curl', 'git-core', 'zlib1g-dev', 'bison' ]
 package [ 'libxml2-dev', 'libxslt1-dev', 'libcurl4-openssl-dev' ]
-package [ 'nodejs', 'libsqlite3-dev', 'sqlite3' ]
+package [ 'nodejs', 'libsqlite3-dev', 'sqlite3', 'build-essential' ]
 
 ###
 # Install Ruby Runtime
 #
-# Note: Trying out a 'supermarket' cookbook per other 'optional' exercise.
-#       Not 100% clear if this is the correct way to do that
+# Note:  Using a 'supermarket' cookbook per other 'optional' exercise.
+# Note2: The instructions are to install Ruby2.1 but that is EOL/EOS
+#        I am going to use 2.3 instead
+#        However, this requires updating some of the gems in the app itself
+#              specifically eventmachine needs to be at least 1.0.4
+#                           json needs to be 1.8.3
+#        As defined the app will not work with Ruby 2.2+
 ###
 ruby_runtime 'middleman' do
 	version '2.3'
@@ -90,8 +98,44 @@ end
 service 'apache2' do
 	action [:enable, :start, :restart]
 end
+###
+# Configure Middleman
+#   -install Bundler
+#   -create a new user (middleman)
+#   -clone the repo https://github.com/learnchef/middleman-blog.git
+#   -modify Gemfile.lock for Ruby 2.3 compatibility 
+#   -bundle install 
+###
+gem_package "bundler"
 
+user 'middleman' do
+	comment 'User to specifically run middleman app'
+	home '/home/middleman'
+	shell '/bin/bash'
+	manage_home true
+	group 'sudo'    # Should grant sudo which seems to be necessary for bundle-install
+	                # Not exactly the best solution.
+end
 
+git '/home/middleman/middleman-blog' do
+	repository 'https://github.com/learnchef/middleman-blog.git'
+	user 'middleman'
+	action :sync
+end
 
+cookbook_file '/home/middleman/middleman-blog/Gemfile.lock' do
+	source 'middleman-gemfilelock'
+	owner 'middleman'
+end
 
+execute "bundle-install" do
+  	user "middleman"
+	cwd "/home/middleman/middleman-blog/"
+	command "bundle install"
+        action :run
+end
+
+####
+# Setup thin service
+####
 
