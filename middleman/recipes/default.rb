937e24519a7cf7ec1287512a8c3f5bdfc70da349
Robert Whitcher <whitcher_robert@yahoo.com>
2017-11-29 08:44:20 -0600

    Woking version of the Middleman exercise
    Updated for Ruby 2.3

--- middleman/recipes/default.rb
+++ middleman/recipes/default.rb
@@ -111,7 +111,7 @@ gem_package "bundler"
 user 'middleman' do
 	comment 'User to specifically run middleman app'
 	home '/home/middleman'
-	shell '/bin/bash'
+	shell '/usr/sbin/nologin'
 	manage_home true
 	group 'sudo'    # Should grant sudo which seems to be necessary for bundle-install
 	                # Not exactly the best solution.
@@ -138,4 +138,34 @@ end
 ####
 # Setup thin service
 ####
+#
+# Two vars used in the template files
+node.default['project_home_dir'] = '/home/middleman'
+node.default['project_install_directory'] = '/home/middleman/middleman-blog'
+
+bash 'setup_thin' do
+	cwd '/home/middleman/middleman-blog'
+	code <<-EOH
+	       thin install
+	       /usr/sbin/update-rc.d -f thin defaults
+	       EOH
+end
+
+template '/etc/thin/blog.yml' do
+	source 'thin.blog.yml.erb'
+	owner  'root'
+	group  'root'
+	mode   '0644'
+end
+
+template '/etc/init.d/thin' do
+	source 'thin.initd.erb'
+	owner  'root'
+	group  'root'
+	mode   '0755'
+end
+
+service 'thin' do
+	action :restart
+end
 
